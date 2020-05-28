d2b095bd4aa2d8b234b24cd3c9b73dacfaf4dd31
Alex Karet <alex.karet@softwire.com>
2020-05-28 11:32:21 +0100

    Add ability to add task and mark as complete or uncomplete

--- app.py
+++ app.py
@@ -4,10 +4,38 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
+@app.route('/', methods=['POST'])
+def add_title():
+    session.add_item(request.form.get('title'))
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
+    def status_sort(item):
+        if item['status'] == "Not Started":
+            return 1
+        else:
+            return 2
     items = session.get_items()
-    return render_template('index.html', items=items)
+    return render_template('index.html', items=sorted(items, key=status_sort))
+
+
+@app.route('/update_items', methods=['POST'])
+def update_items():
+    for item in session.get_items():
+        print(item)
+        item_completed = request.form.get(str(item['id']))
+        print(item_completed)
+        if item_completed == "on":
+            item['status'] = "Completed"
+            session.save_item(item)
+        if item_completed is None:
+            item['status'] = "Not Started"
+            session.save_item(item)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
