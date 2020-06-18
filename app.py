db7388fbde097da397fc8bd222e501a063904c59
Alex Karet <alex.karet@softwire.com>
2020-06-18 11:09:43 +0100

    Get and display cards from trello

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+from trello.helpers import Trello
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
+trello_board = Trello("6yxcx50y")
 
 
 @app.route('/', methods=['POST'])
@@ -13,13 +15,13 @@ def add_title():
 
 @app.route('/')
 def index():
-    items = session.get_items()
+    items = trello_board.get_items()
     return render_template('index.html', items=sorted(items, key=lambda item: item['status'], reverse=True))
 
 
 @app.route('/update_items', methods=['POST'])
 def update_items():
-    for item in session.get_items():
+    for item in trello_board.get_items():
         item_completed = request.form.get(str(item['id']))
         if item_completed == "on":
             item['status'] = "Completed"
--- trello/helpers.py
+++ trello/helpers.py
@@ -37,12 +37,34 @@ class Trello:
         return (response.json())
 
     def get_cards_from_board(self):
-        print(self._query_trell_boards("cards"))
+        return self._query_trell_boards("cards")
 
-    def get_lists_from_board(self):
-        print(self._query_trell_boards("lists"))
+    def _get_lists_from_board(self):
+        lists = self._query_trell_boards("lists")
+        todo_list_id = ""
+        completed_list_id = ""
+        for list_items in lists:
+            if list_items["name"] == "Not Started":
+                todo_list_id = list_items["id"]
+                continue
+            if list_items["name"] == "Completed":
+                completed_list_id = list_items["id"]
+                continue
+        return todo_list_id, completed_list_id
+
+    def get_items(self):
+        todo_list_id, completed_list_id = self._get_lists_from_board()
+        items = []
+        for card in self.get_cards_from_board():
+            if card["idList"] == todo_list_id:
+                status = "Not Started"
+            elif card["idList"] == completed_list_id:
+                status = "Completed"
+            else:
+                raise ValueError(f"{card['name']} is not a memeber of a valid Todo list")
+            items.append({ 'id': card["idShort"], 'status': status, 'title': card["name"] })
+        return items
+
 
 if __name__ == "__main__":
     trello_api = Trello("6yxcx50y")
-    trello_api.get_cards_from_board()
-    trello_api.get_lists_from_board()
