1dbd2ab8636738e62b9ca87050e159e162946466
Alex Karet <alex.karet@softwire.com>
2020-06-18 10:56:34 +0100

    Get lists from board
    Refactor to tidy up board api calls

--- trello/helpers.py
+++ trello/helpers.py
@@ -6,13 +6,14 @@ class Trello:
     key = ""
     token = ""
 
-    def __init__(self):
+    def __init__(self, board_id):
         with open("secrets/trello_secrets.json", "r") as json_file:
             data = json.load(json_file)
             self.key = data["key"]
             self.token = data["token"]
+        self.board_id = board_id
 
-    def get_query(self, **kwargs):
+    def _get_query(self, **kwargs):
         query = {
             "key": self.key,
             "token": self.token
@@ -21,21 +22,27 @@ class Trello:
             query[key] = value
         return query
 
-    def get_all_cards_from_board(self, board_id):
-        url = f"https://api.trello.com/1/boards/{board_id}/cards"
+    def _query_trell_boards(self, endpoint, **kwargs):
+        url = f"https://api.trello.com/1/boards/{self.board_id}/{endpoint}"
         headers = {
-        "Accept": "application/json"
+            "Accept": "application/json"
         }
         response = requests.request(
-        "GET",
-        url,
-        headers=headers,
-        params=self.get_query()
+            "GET",
+            url,
+            headers=headers,
+            params=self._get_query(**kwargs)
         )
 
-        print(response.json())
+        return (response.json())
 
+    def get_cards_from_board(self):
+        print(self._query_trell_boards("cards"))
+
+    def get_lists_from_board(self):
+        print(self._query_trell_boards("lists"))
 
 if __name__ == "__main__":
-    trello_api = Trello()
-    print(trello_api.get_all_cards_from_board("6yxcx50y"))
+    trello_api = Trello("6yxcx50y")
+    trello_api.get_cards_from_board()
+    trello_api.get_lists_from_board()
