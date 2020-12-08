2cab237b8524bc9252598a6959b911b1899c92d4
Alex Karet <alex.karet@softwire.com>
2020-12-08 18:04:24 +0000

    Correct app name

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -6,8 +6,7 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         heroku container:login
-        echo "here"
         docker push registry.heroku.com/karro96-todo-app/web
-        heroku container:release web --app karro
+        heroku container:release web --app karro96-todo-app
     fi
 fi
