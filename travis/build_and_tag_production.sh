25deec04ce9bfb0e57e885afe6ca24ea4e54fa87
Alex Karet <alex.karet@softwire.com>
2020-12-08 18:08:55 +0000

    adjusted with correct queries for master.

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -4,7 +4,7 @@ set -ev
 docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
         docker tag karrot96/todo-app:$TRAVIS_COMMIT karrot96/todo-app:latest
         docker push karrot96/todo-app:latest
     fi
--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -2,7 +2,7 @@
 
 set -ev
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         heroku container:login
