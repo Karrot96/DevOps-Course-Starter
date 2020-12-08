91a605543bf80799bd38391bdb75a6152f77a6af
Alex Karet <alex.karet@softwire.com>
2020-12-08 11:19:38 +0000

    Set branch for deployment to PR branch for now

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -3,6 +3,6 @@
 set -ev
 docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "$TRAVIS_BRANCH" == "exercise8" ]]; then
     docker push karrot96/ToDoApp:latest
 fi
--- travis/deploy_to_docker.sh
+++ travis/deploy_to_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash sh
 
 set -ev
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "$TRAVIS_BRANCH" == "exercise8" ]]; then
     docker pull karrot96/todo-app:latest
     docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
     docker push registry.heroku.com/karro96-todo-app/web
