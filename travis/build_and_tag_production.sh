e897aa27f7bc9cd7a6146d25d27f3abd17e672ea
Alex Karet <alex.karet@softwire.com>
2020-12-08 11:43:07 +0000

    Test branching

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -3,6 +3,7 @@
 set -ev
 docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
+echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "exercise8" ]]; then
     docker push karrot96/ToDoApp:latest
 fi
