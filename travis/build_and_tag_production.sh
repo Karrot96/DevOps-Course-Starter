3100d6461e4c9530eaf62f72228e415adb1ff192
Alex Karet <alex.karet@softwire.com>
2020-12-08 12:43:12 +0000

    Fix names for docker hub

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -5,6 +5,6 @@ docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-        docker push karrot96/ToDoApp:latest
+        docker push karrot96/todo-app:latest
     fi
 fi
--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 set -ev
-echo $TRAVIS_BRANCH
-echo $TRAVIS_PULL_REQUEST
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    echo $TRAVIS_BRANCH
-    echo $TRAVIS_PULL_REQUEST
     if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-        echo $TRAVIS_BRANCH
-        echo $TRAVIS_PULL_REQUEST
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         docker push registry.heroku.com/karro96-todo-app/web
