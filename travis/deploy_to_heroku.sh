f60639e79f4dcce9eda571051e9cbdaeb9ecb222
Alex Karet <alex.karet@softwire.com>
2020-12-08 12:29:50 +0000

    with echo

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 set -ev
+echo $TRAVIS_BRANCH
+echo $TRAVIS_PULL_REQUEST
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    echo $TRAVIS_BRANCH
+    echo $TRAVIS_PULL_REQUEST
     if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+        echo $TRAVIS_BRANCH
+        echo $TRAVIS_PULL_REQUEST
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         docker push registry.heroku.com/karro96-todo-app/web
