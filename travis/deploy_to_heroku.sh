d696216639328a3bdacbad253511b76e5279942a
Alex Karet <alex.karet@softwire.com>
2020-12-08 17:59:18 +0000

    Better ordering

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -5,9 +5,6 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
-        echo "here"
-        heroku login
-        echo "here"
         heroku container:login
         echo "here"
         docker push registry.heroku.com/karro96-todo-app/web
