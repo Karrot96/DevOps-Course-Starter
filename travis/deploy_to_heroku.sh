31dfec92e3e437401ea560e11b7c8396e3522569
Alex Karet <alex.karet@softwire.com>
2020-12-08 17:23:30 +0000

    heroku login

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -6,7 +6,7 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         docker pull karrot96/todo-app:latest
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         docker push registry.heroku.com/karro96-todo-app/web
-        docker login --username=_ --password=$HEROKU_TOKEN registry.heroku.com
+        heroku login 
         heroku container:release web
     fi
 fi
