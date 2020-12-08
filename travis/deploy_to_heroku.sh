9e336118a0ad0bf73db786236b1dc029701c46cd
Alex Karet <alex.karet@softwire.com>
2020-12-08 17:36:52 +0000

    Specify app

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -7,6 +7,6 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
         docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
         docker push registry.heroku.com/karro96-todo-app/web
         heroku container:login
-        heroku container:release web
+        heroku container:release web --app karro96-todo-app
     fi
 fi
