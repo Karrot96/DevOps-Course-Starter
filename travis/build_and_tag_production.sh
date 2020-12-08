12ddad4003e354ab95e10b691ff8eb4bece32026
Alex Karet <alex.karet@softwire.com>
2020-12-08 12:25:00 +0000

    Fix conditionals

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -3,6 +3,8 @@
 set -ev
 docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
-if [[ "$TRAVIS_BRANCH" == "master" && $TRAVIS_PULL_REQUEST == "true"]]; then
-    docker push karrot96/ToDoApp:latest
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+        docker push karrot96/ToDoApp:latest
+    fi
 fi
--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -ev
-if [[ "$TRAVIS_BRANCH" == "master" && $TRAVIS_PULL_REQUEST == "true"]]; then
-    docker pull karrot96/todo-app:latest
-    docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
-    docker push registry.heroku.com/karro96-todo-app/web
-    docker login --username=alex.karet@softwire.com --password=$HEROKU_TOKEN registry.heroku.com
-    heroku container:release web
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+        docker pull karrot96/todo-app:latest
+        docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
+        docker push registry.heroku.com/karro96-todo-app/web
+        docker login --username=alex.karet@softwire.com --password=$HEROKU_TOKEN registry.heroku.com
+        heroku container:release web
+    fi
 fi
