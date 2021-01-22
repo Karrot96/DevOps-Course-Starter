0af1e86f81b4f2ba2ee8cccb7132748b5e401070
Alex Karet <alex.karet@softwire.com>
2021-01-22 11:03:58 +0000

    Test No branching

--- travis/deploy_to_heroku.sh
+++ travis/deploy_to_heroku.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 set -ev
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-        docker pull karrot96/todo-app:latest
-        docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
-        heroku container:login
-        docker push registry.heroku.com/karro96-todo-app/web
-        heroku container:release web --app karro96-todo-app
-    fi
-fi
+docker pull karrot96/todo-app:latest
+docker tag karrot96/todo-app:latest registry.heroku.com/karro96-todo-app/web
+heroku container:login
+docker push registry.heroku.com/karro96-todo-app/web
+heroku container:release web --app karro96-todo-app
