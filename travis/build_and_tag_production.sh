3cccf009bb8ad9b3b67f1d56ce49ca35e452f77a
Alex Karet <alex.karet@softwire.com>
2021-01-29 15:17:19 +0000

    Only release latest on master

--- travis/build_and_tag_production.sh
+++ travis/build_and_tag_production.sh
@@ -3,5 +3,9 @@
 set -ev
 docker build --target production --tag karrot96/todo-app:$TRAVIS_COMMIT --cache-from karrot96/todo-app .
 docker push karrot96/todo-app:$TRAVIS_COMMIT
-docker tag karrot96/todo-app:$TRAVIS_COMMIT karrot96/todo-app:latest
-docker push karrot96/todo-app:latest
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+        docker tag karrot96/todo-app:$TRAVIS_COMMIT karrot96/todo-app:latest
+        docker push karrot96/todo-app:latest
+    fi
+fi
