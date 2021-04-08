d3f49f86b4be0e43a6ae3a6b6c7d8dc0bdce0fc2
Alex Karet <alex.karet@softwire.com>
2021-04-08 15:54:10 +0100

    flask decorator

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,11 +6,13 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from todo_app.models.view_model import ViewModel
 from flask_login import LoginManager, login_required
+from flask_user import roles_required
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"]=os.environ.get("SECRET_KEY")
+    app.config["USER_UNAUTHORIZED_ENDPOINT"] = "/index"
     mongo_database = MongoWrapper(os.environ.get('DEFAULT_DATABASE'))
     login_manager = LoginManager()
     github_authenticator = GithubAuthentication()
@@ -36,11 +38,12 @@ def create_app():
 
     login_manager.init_app(app)
 
-    @check_writer
     @app.route('/', methods=['POST'])
     @login_required
+    @roles_required(Roles.WRITER)
     def add_title():
         mongo_database.add_item(request.form.get('title'))
+        return redirect(url_for('index'))
 
     @app.route('/')
     @login_required
@@ -50,31 +53,34 @@ def create_app():
         role_id = flask_login.current_user.role.value
         return render_template('index.html', view_model=item_view_model, role_id=role_id)
 
-    @check_writer
     @app.route('/complete_items/<id>')
     @login_required
+    @roles_required(Roles.WRITER)
     def complete_items(id):
         mongo_database.complete_item(id)
-        
+        return redirect(url_for('index'))
 
-    @check_writer
     @app.route('/move_todo/<id>')
     @login_required
+    @roles_required(Roles.WRITER)
     def set_doing(id):
         mongo_database.set_doing(id)
+        return redirect(url_for('index'))
 
-    @check_writer
     @app.route('/undo_complete/<id>')
     @login_required
+    @roles_required(Roles.WRITER)
     def undo_complete(id):
         mongo_database.set_todo(id)
+        return redirect(url_for('index'))
 
 
-    @check_writer
     @app.route('/create_board/<name>')
     @login_required
+    @roles_required(Roles.WRITER)
     def create_board(name):
         mongo_database.create_board(name)
+        return redirect(url_for('index'))
 
     @app.route('/login/callback', methods=["GET"])
     def github_authentication():
